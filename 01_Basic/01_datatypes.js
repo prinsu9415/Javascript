4140f3d0daefa09fbd89eb4d2a2d021249529e04
prinsu9415 <118649692+prinsu9415@users.noreply.github.com>
2023-10-03 15:58:08 +0000

    DataTypes

---
+++ 01_Basic/01_datatypes.js
@@ -0,0 +1,16 @@
+let name ="Prince"
+let age = 23
+let numberId = false
+let rollNumber = null 
+
+//number
+//string
+//boolean ture/false
+//null standalone value
+//undefined
+//Symbol unique
+
+//object
+
+//console.log(typeof null )
+console.table([typeof age ,typeof undefined ,typeof numberId ,typeof "Prince"])
