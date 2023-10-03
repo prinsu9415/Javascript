3fa8fff7769207d68c1331a3560281e900584ae6
prinsu9415 <118649692+prinsu9415@users.noreply.github.com>
2023-10-03 15:18:07 +0000

    variables

---
+++ 01_Basic/01_Variables.js
@@ -0,0 +1,11 @@
+const rollNumber = 124
+let emainId = "pri1233@gmail.com"
+var yourPasssword = "f3214"
+// rollNumber =120   not allowed
+emainId = "prins3424@gmail.com"
+yourPasssword = "3681268"
+let randmNumber;
+
+console.table([rollNumber,emainId,yourPasssword,randmNumber])
+// use table for display multiple variables
+// prefer const and let insteed of var
