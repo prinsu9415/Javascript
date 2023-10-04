efaef2b5a776a662b3d33b44faf0724cf470ae36
prinsu9415 <118649692+prinsu9415@users.noreply.github.com>
2023-10-04 05:01:22 +0000

    datatypes  comparision

---
+++ 01_Basic/01_comparision.js
@@ -0,0 +1,20 @@
+console.log(2 > 1);
+console.log(2 != 1)
+console.log(2 ==  1 )  // normal comparision
+
+console.log("2" > 1 )   //try to compare same datatypes
+console.log("02" > 1)
+
+// null
+console.log(null > 0)
+console.log(null == 0);
+console.log( null >= 0);
+
+//undefined
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log( undefined >=0);
+
+// ===
+console.log("2" === 2); // this compare  dataTypes and  Values
+
