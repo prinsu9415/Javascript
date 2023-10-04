cee6f0574a31f83c371d0499f202a2efc5f3b50d
prinsu9415 <118649692+prinsu9415@users.noreply.github.com>
2023-10-04 05:00:38 +0000

    datatypes theory

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,26 @@
+
+ // premitive types
+ // 77 types : string , number ,boolean , null , undefined ,symbol , Bigint
+ 
+ //non-premitive
+ //Array , Object , function
+
+
+ // symbol
+ let loggedIn = Symbol('123')  
+ let  numbeId = Symbol('123')
+
+ console.log(loggedIn === numbeId);
+
+
+ // function
+ let myFunction = function(){
+    console.log("Hello world");
+ }
+
+ // object
+
+ let myobj1 = {
+    myName : "prince",
+    myag: 18,
+ }
