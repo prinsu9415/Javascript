c9fd39ab86fb6ece7812d8af9f5118acaf3210c6
prinsu9415 <118649692+prinsu9415@users.noreply.github.com>
2023-10-05 04:51:23 +0000

    string

---
+++ 01_Basic/01_strings.js
@@ -0,0 +1,13 @@
+const name = "Prince"
+const repoCount = 12
+
+//console.log('Hello  my name is ${name} and my repo count is ${repoCount}');
+//console.log(name.toLocaleUpperCase());
+
+let newString = new String('Prince')
+
+//console.log(name.length);
+//console.log(name.replace('Prince','Prinsu'));
+//console.log(name[0]);
+console.log(name.indexOf('i'));
+console.log(name.charAt(2));
