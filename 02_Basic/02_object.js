c58d971bafd585ebebcb4da95d7b3a174281a40f
prinsu9415 <118649692+prinsu9415@users.noreply.github.com>
2023-10-05 13:02:21 +0000

    object part 2

---
+++ 02_Basic/02_object.js
@@ -0,0 +1,30 @@
+ const instagram = {}
+
+ instagram.email = "sjdhj@gmail.com"
+ instagram.id = 123
+ instagram.loggedIn = false
+
+ // console.log(instagram);
+ // console.log(Object.keys(instagram)); // To access the key of an object
+// console.log(Object.values(instagram)); // To access the values of an object
+
+
+ const myObj1 = {
+    email : "dfhsddjfjds@.com",
+    fullname : {
+                 userfullname : {
+                             firstname : "Prince",
+                             lastname : "Yadav"
+                 }
+    }
+ }
+ // console.log(myObj1.fullname.userfullname.firstname); // if you want call name 
+
+ const obj1 = {1 : "A", 2 : "B"}
+ const obj2 = {3 : "C", 4 : "D"}
+
+ const obj3 = Object.assign(obj1,obj2)
+ console.log(obj3);
+// 
+const obj4 = Object.assign({},obj1,obj2)
+console.log(obj4);
