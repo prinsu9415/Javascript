29f4fe5be6cf0d5b562dab5f37884703aab9f82f
prinsu9415 <118649692+prinsu9415@users.noreply.github.com>
2023-10-07 05:20:15 +0000

    IIFE

---
+++ 02_Basic/01_iife.js
@@ -0,0 +1,9 @@
+// Immediately Invoked Function Expression
+
+(function mera (){
+    console.log("Jay Hind Doston"); // Named IIFE
+})();
+
+(()=>{
+     console.log("Jay Shree Ram");  // UnNamed IIFE
+} )()
