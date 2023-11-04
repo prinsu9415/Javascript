105f154d2dcf13f4274c279bf49985c8ebf44401
prinsu9415 <118649692+prinsu9415@users.noreply.github.com>
2023-11-04 15:20:48 +0530

    Update class3.js

---
+++ Training Javascript/class3.js
@@ -0,0 +1,22 @@
+// loops
+// for loops
+
+for(let i = 1 ;i <=10 ; i++) {
+    // console.log(i)
+ } 
+ 
+ // While loop
+ let j = 1
+ while(j<=10){
+     //console.log(j)
+     j++;
+ }
+ // Do While
+  let k =1;
+  do{
+     // console.log(k)
+     k++;
+  }
+  while(k<=10)
+ 
+  
