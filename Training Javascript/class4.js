f5ea726bb36397e80475a5fcd2eaa018716966a3
prinsu9415 <118649692+prinsu9415@users.noreply.github.com>
2023-11-05 19:09:29 +0530

    Add files via upload

---
+++ Training Javascript/class4.js
@@ -0,0 +1,25 @@
+const a = 20
+const b = '20'
+console.log(typeof(b))
+if(a == b)
+{
+    console.log("a is  equal to b")
+}
+else{
+    console.log("I cant say anything")
+}
+
+
+const str1 = 'hello'
+const str2 = 'hello'
+
+if(str1 > str2)
+{
+    console.log("str1 is greater than str2")
+}
+else if(str1 == str2){
+    console.log("str1 is equal to str2")
+}
+else{
+    console.log("str2 is greater than str1")
+}
