72101d9d6079601dce0b5b84a71cadf9617d26fd
prinsu9415 <118649692+prinsu9415@users.noreply.github.com>
2023-11-06 17:29:13 +0530

    Add files via upload

---
+++ Training Javascript/class5.js
@@ -0,0 +1,20 @@
+// Array
+var x = [1,2,3,4,5]
+
+console.log(x)
+console.log(x[0])
+for(let i = 0; i<x.length; i++){
+    
+    console.log(i+" : "+x[i])
+}
+
+//JSON OBJ
+var json = {'name':'prince','city':'Azamgarh','id':123,'age':23} //this a simple object
+// console.log(json.name)
+ var json1 = [{'name':'prince','city':'Azamgarh','id':123,'age':23},
+              {'name':'Akhil','city':'Prayagraj','id':124,'age':22},
+              {'name':'Saurabh','city':'Noida','id':125,'age':24}]
+      
+  console.log(json1)
+    console.log(json1[1].name)          
+              
